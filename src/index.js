d11a0fe65813f0a69b4de23a6dcd4381060a47c8
NaveenNextbrain <naveenkumar16611661@gmail.com>
2025-11-20 19:54:36 +0530

    i will add index.js my name

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
-  res.end('Hello from myapp\n');
+  res.end('Hello Naveen this is CI/CD Demo apps\n');
 });
 server.listen(port, () => console.log(`listening ${port}`));
