65913785bb6711954067d8b51a53d470926654ef
NaveenNextbrain <naveenkumar16611661@gmail.com>
2025-11-20 16:58:18 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// src/index.js
+const http = require('http');
+const port = process.env.PORT || 3000;
+const server = http.createServer((req, res) => {
+  res.end('Hello from myapp\n');
+});
+server.listen(port, () => console.log(`listening ${port}`));
---
+++ test/app.test.js
@@ -0,0 +1,4 @@
+// test/app.test.js
+test('sum', () => {
+  expect(1 + 2).toBe(3);
+});
