68637ed5a745e589153decb497c139072aa877b0
NaveenNextbrain <naveenkumar16611661@gmail.com>
2025-11-21 11:08:26 +0530

    we change index.js file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
-  res.end('Hello Naveen this is CI/CD Demo apps\n');
+  res.end('Hello Naveen, this is the CI/CD demo app\n');
 });
 server.listen(port, () => console.log(`listening ${port}`));
